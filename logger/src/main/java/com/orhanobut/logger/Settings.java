022e977ba612518b5d4500496bb4d93b134e4b5c
orhanobut <orhanobut@gmail.com>
2015-08-02 11:53:30 +0200

    tests

--- logger/src/main/java/com/orhanobut/logger/Settings.java
+++ logger/src/main/java/com/orhanobut/logger/Settings.java
@@ -5,48 +5,51 @@ package com.orhanobut.logger;
  */
 public final class Settings {
 
-    private int methodCount = 2;
-    private boolean showThreadInfo = true;
-    private int methodOffset = 0;
+  private int methodCount = 2;
+  private boolean showThreadInfo = true;
+  private int methodOffset = 0;
 
-    /**
-     * Determines how logs will printed
-     */
-    private LogLevel logLevel = LogLevel.FULL;
+  /**
+   * Determines how logs will printed
+   */
+  private LogLevel logLevel = LogLevel.FULL;
 
-    public Settings hideThreadInfo() {
-        showThreadInfo = false;
-        return this;
+  public Settings hideThreadInfo() {
+    showThreadInfo = false;
+    return this;
+  }
+
+  public Settings setMethodCount(int methodCount) {
+    if (methodCount < 0) {
+      methodCount = 0;
     }
+    this.methodCount = methodCount;
+    return this;
+  }
 
-    public Settings setMethodCount(int methodCount) {
-        this.methodCount = methodCount;
-        return this;
-    }
+  public Settings setLogLevel(LogLevel logLevel) {
+    this.logLevel = logLevel;
+    return this;
+  }
 
-    public Settings setLogLevel(LogLevel logLevel) {
-        this.logLevel = logLevel;
-        return this;
-    }
+  public Settings setMethodOffset(int offset) {
+    this.methodOffset = offset;
+    return this;
+  }
 
-    public Settings setMethodOffset(int offset) {
-        this.methodOffset = offset;
-        return this;
-    }
+  public int getMethodCount() {
+    return methodCount;
+  }
 
-    public int getMethodCount() {
-        return methodCount;
-    }
+  public boolean isShowThreadInfo() {
+    return showThreadInfo;
+  }
 
-    public boolean isShowThreadInfo() {
-        return showThreadInfo;
-    }
+  public LogLevel getLogLevel() {
+    return logLevel;
+  }
 
-    public LogLevel getLogLevel() {
-        return logLevel;
-    }
-
-    public int getMethodOffset() {
-        return methodOffset;
-    }
+  public int getMethodOffset() {
+    return methodOffset;
+  }
 }
---
+++ logger/src/test/java/com.orhanobut.logger/SettingsTest.java
@@ -0,0 +1,82 @@
+package com.orhanobut.logger;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Orhan Obut
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class SettingsTest extends TestCase {
+
+  private Settings settings;
+
+  @Before
+  public void setup() {
+    settings = new Settings();
+  }
+
+  @After
+  public void tearDown() {
+    settings = null;
+  }
+
+  @Test
+  public void testDefaultShowThreadInfo() {
+    assertThat(settings.isShowThreadInfo()).isTrue();
+  }
+
+  @Test
+  public void testCustomShowThreadInfo() {
+    settings.hideThreadInfo();
+    assertThat(settings.isShowThreadInfo()).isFalse();
+  }
+
+  @Test
+  public void testDefaultLogLevel() {
+    assertThat(settings.getLogLevel()).isEqualTo(LogLevel.FULL);
+  }
+
+  @Test
+  public void testCustomLogLevel() {
+    settings.setLogLevel(LogLevel.NONE);
+    assertThat(settings.getLogLevel()).isEqualTo(LogLevel.NONE);
+
+    settings.setLogLevel(LogLevel.FULL);
+    assertThat(settings.getLogLevel()).isEqualTo(LogLevel.FULL);
+  }
+
+  @Test
+  public void testMethodCount() {
+    //default
+    assertThat(settings.getMethodCount()).isEqualTo(2);
+
+    settings.setMethodCount(4);
+    assertThat(settings.getMethodCount()).isEqualTo(4);
+
+    //negative values should be convert to 0
+    settings.setMethodCount(-10);
+    assertThat(settings.getMethodCount()).isEqualTo(0);
+  }
+
+  @Test
+  public void testMethodOffset() {
+    //default
+    assertThat(settings.getMethodOffset()).isEqualTo(0);
+
+    settings.setMethodOffset(10);
+    assertThat(settings.getMethodOffset()).isEqualTo(10);
+
+    settings.setMethodOffset(-10);
+    assertThat(settings.getMethodOffset()).isEqualTo(-10);
+  }
+}
