3822fb64e22d91542313a4213857de47bd5970b8
Orhan Obut <orhan.obut@blacklane.com>
2017-05-26 17:28:20 +0200

    Add more sample codes

--- sample/src/main/java/com/orhanobut/sample/MainActivity.java
+++ sample/src/main/java/com/orhanobut/sample/MainActivity.java
@@ -2,8 +2,11 @@ package com.orhanobut.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.DiskLogAdapter;
+import com.orhanobut.logger.FormatStrategy;
 import com.orhanobut.logger.Logger;
 import com.orhanobut.logger.PrettyFormatStrategy;
 
@@ -18,15 +21,35 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    Logger.addAdapter(new AndroidLogAdapter());
+    Log.d("Tag", "I'm a log which you don't see easily, hehe");
+    Log.d("json content", "{ \"key\": 3, \n \"value\": something}");
+    Log.d("error", "There is a crash somewhere or any warning");
+
+    Logger.addLogAdapter(new AndroidLogAdapter());
     Logger.d("message");
 
     Logger.clearLogAdapters();
-    PrettyFormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
-        .showThreadInfo(false)
+
+
+    FormatStrategy formatStrategy = PrettyFormatStrategy.newBuilder()
+        .showThreadInfo(false)  // (Optional) Whether to show thread info or not. Default true
+        .methodCount(0)         // (Optional) How many method line to show. Default 2
+        .methodOffset(3)        // (Optional) Skips some method invokes in stack trace. Default 5
+//        .logStrategy(customLog) // (Optional) Changes the log strategy to print out. Default LogCat
+        .tag("My custom tag")   // (Optional) Custom tag for each log. Default PRETTY_LOGGER
         .build();
 
-    Logger.addAdapter(new AndroidLogAdapter(formatStrategy));
+    Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
+
+    Logger.addLogAdapter(new AndroidLogAdapter(){
+      @Override public boolean isLoggable(int priority, String tag) {
+        return BuildConfig.DEBUG;
+      }
+    });
+
+    Logger.addLogAdapter(new DiskLogAdapter());
+
+
     Logger.w("no thread info and only 1 method");
 
     Logger.clearLogAdapters();
@@ -35,7 +58,7 @@ public class MainActivity extends Activity {
         .methodCount(0)
         .build();
 
-    Logger.addAdapter(new AndroidLogAdapter(formatStrategy));
+    Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
     Logger.i("no thread info and method info");
 
     Logger.t("tag").e("Custom tag for only one use");
@@ -56,7 +79,7 @@ public class MainActivity extends Activity {
         .methodCount(0)
         .tag("MyTag")
         .build();
-    Logger.addAdapter(new AndroidLogAdapter(formatStrategy));
+    Logger.addLogAdapter(new AndroidLogAdapter(formatStrategy));
 
     Logger.w("my log message with my tag");
   }
