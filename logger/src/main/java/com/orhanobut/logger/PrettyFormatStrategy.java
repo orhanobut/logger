ced1530dc3bbffcbbcb72277db84781ff6bf9e66
Orhan Obut <orhan.obut@blacklane.com>
2017-05-26 10:29:40 +0200

    Increase the length of dividers

--- logger/src/main/java/com/orhanobut/logger/PrettyFormatStrategy.java
+++ logger/src/main/java/com/orhanobut/logger/PrettyFormatStrategy.java
@@ -21,8 +21,8 @@ public class PrettyFormatStrategy implements FormatStrategy {
   private static final char BOTTOM_LEFT_CORNER = '╚';
   private static final char MIDDLE_CORNER = '╟';
   private static final char HORIZONTAL_DOUBLE_LINE = '║';
-  private static final String DOUBLE_DIVIDER = "════════════════════════════════════════════";
-  private static final String SINGLE_DIVIDER = "────────────────────────────────────────────";
+  private static final String DOUBLE_DIVIDER = "════════════════════════════════════════════════════════";
+  private static final String SINGLE_DIVIDER = "────────────────────────────────────────────────────────";
   private static final String TOP_BORDER = TOP_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER;
   private static final String BOTTOM_BORDER = BOTTOM_LEFT_CORNER + DOUBLE_DIVIDER + DOUBLE_DIVIDER;
   private static final String MIDDLE_BORDER = MIDDLE_CORNER + SINGLE_DIVIDER + SINGLE_DIVIDER;
