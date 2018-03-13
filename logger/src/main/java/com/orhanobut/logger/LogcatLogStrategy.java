e3a7ec7594efbdb9a99a1727c415e22ff1d97725
Sven Jacobs <github@svenjacobs.com>
2018-03-13 10:02:44 +0100

    Add default tag to LogcatLogStrategy if tag is null

--- logger/src/main/java/com/orhanobut/logger/LogcatLogStrategy.java
+++ logger/src/main/java/com/orhanobut/logger/LogcatLogStrategy.java
@@ -8,10 +8,15 @@ import static com.orhanobut.logger.Utils.checkNotNull;
 
 public class LogcatLogStrategy implements LogStrategy {
 
+  static final String DEFAULT_TAG = "NO_TAG";
+
   @Override public void log(int priority, @Nullable String tag, @NonNull String message) {
     checkNotNull(message);
 
+    if (tag == null) {
+      tag = DEFAULT_TAG;
+    }
+
     Log.println(priority, tag, message);
   }
-
 }
