66032ffcba9f27f75455f5abd5af5e1659c7890d
balajisunku <balaji.march31@gmail.com>
2021-03-22 18:56:01 +0530

    Migrated to AndroidX

--- logger/src/main/java/com/orhanobut/logger/DiskLogStrategy.java
+++ logger/src/main/java/com/orhanobut/logger/DiskLogStrategy.java
@@ -3,8 +3,8 @@ package com.orhanobut.logger;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.io.FileWriter;
--- logger/src/main/java/com/orhanobut/logger/LoggerPrinter.java
+++ logger/src/main/java/com/orhanobut/logger/LoggerPrinter.java
@@ -1,7 +1,7 @@
 package com.orhanobut.logger;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.json.JSONArray;
 import org.json.JSONException;
